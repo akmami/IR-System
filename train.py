38d3b5d5c6c1ebae66b80355dc6e1c7fdddb7189
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2023-11-21 00:40:25 +0300

    model params modified, bin saving of model, log

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ def train():
     cores = multiprocessing.cpu_count()
     
     # Create model
-    w2v_model = Word2Vec(epochs=30, min_count=20, window=2, vector_size=10, sample=6e-5, alpha=0.03, min_alpha=0.0007, negative=20, workers=cores-1)
+    w2v_model = Word2Vec(epochs=10, min_count=20, window=2, vector_size=100, sample=6e-5, alpha=0.03, min_alpha=0.0007, negative=20, workers=cores-1)
     
     # Build vocabluary.
     # This is new in Word3Vec 4.0.0
@@ -22,8 +22,13 @@ def train():
     # Train model
     w2v_model.train(documents, total_examples=len(documents), epochs=5, report_delay=1)
 
-    # save model
-    w2v_model.save('model/word2vec.model')
+    logging.getLogger().setLevel(logging.INFO)
+    # Keys
+    logging.info("Terms count: {}".format(len(w2v_model.wv.key_to_index)))
 
+    # save model
+    w2v_model.wv.save_word2vec_format("model/word2vec.model", binary=True)
+
+    logging.getLogger().setLevel(logging.WARN)
 
 train()
