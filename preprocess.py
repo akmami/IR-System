9fc9b6e987e89636f0eb72c75dc84ea44b2e0437
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2023-11-21 08:11:46 +0300

    code refactor

--- preprocess.py
+++ preprocess.py
@@ -12,11 +12,15 @@ logging.getLogger().setLevel(logging.INFO)
 cwd = os.getcwd()
 
 
+# Define NLP functions to be done in preprocess_string
+filters = [lambda x: x.lower(), strip_tags, strip_punctuation, strip_multiple_whitespaces, strip_numeric, remove_stopwords, strip_short, stem_text]
+
+
 # check whether datasets exists
 def check_dataset():
-    exists = os.path.exists(os.path.join(cwd, "dataset/documents.csv"))
-    exists = exists and os.path.exists(os.path.join(cwd, "dataset/ground_truth.csv"))
-    exists = exists and os.path.exists(os.path.join(cwd, "dataset/queries.csv"))
+    exists = os.path.exists(os.path.join(cwd, "dataset", "documents.csv"))
+    exists = exists and os.path.exists(os.path.join(cwd, "dataset", "ground_truth.csv"))
+    exists = exists and os.path.exists(os.path.join(cwd, "dataset", "queries.csv"))
 
     if not exists:
         logging.error("Dataset is not complete. Under the dataset directory, there should be documents.csv, ground_truth.csv and queries.csv")
@@ -26,13 +30,13 @@ def check_dataset():
 
 
 # Read dataset
-def read_dataset(onlyColumn=False, preprocess=True):
+def read_dataset(onlyColumn=False):
     
     check_dataset()
 
-    documents = pd.read_csv("dataset/documents.csv") 
-    ground_truth = pd.read_csv("dataset/ground_truth.csv") 
-    queries = pd.read_csv("dataset/queries.csv") 
+    documents = pd.read_csv(os.path.join(cwd, "dataset", "documents.csv"))
+    ground_truth = pd.read_csv(os.path.join(cwd, "dataset", "ground_truth.csv"))
+    queries = pd.read_csv(os.path.join(cwd, "dataset", "queries.csv"))
 
     # Convert NaN entries to ""
     # This avoids the error when gensim function tries to preprocess it.
@@ -50,9 +54,9 @@ def read_dataset(onlyColumn=False, preprocess=True):
 
 
 # Pre-process dataset
-def preprocess(onlyColumn=False, preprocess=False):
+def preprocess(onlyColumn=False):
 
-    (documents, ground_truth, queries) = read_dataset(onlyColumn, preprocess)
+    (documents, ground_truth, queries) = read_dataset(onlyColumn)
 
     '''
     documents["title"] = [stem_text(title) for title in documents["title"]]
@@ -60,16 +64,23 @@ def preprocess(onlyColumn=False, preprocess=False):
     documents["title"] = [remove_stopword_tokens(title, stopwords=STOPWORDS) for title in documents["title"]]
     '''
 
-    if preprocess:
-        # Define NLP functions to be done in preprocess_string
-        filters = [lambda x: x.lower(), strip_tags, strip_punctuation, strip_multiple_whitespaces, strip_numeric, remove_stopwords, strip_short, stem_text]
+    # Tokenize, lowercase and remove stop words for title, author and text columns in documents
+    # documents["title"] = [preprocess_string(title, filters=filters) for title in documents["title"]]
+    # documents["author"] = [preprocess_string(author, filters=filters) for author in documents["author"]]
+    documents = [preprocess_string(text, filters=filters) for text in documents]
 
-        # Tokenize, lowercase and remove stop words for title, author and text columns in documents
-        # documents["title"] = [preprocess_string(title, filters=filters) for title in documents["title"]]
-        # documents["author"] = [preprocess_string(author, filters=filters) for author in documents["author"]]
-        documents = [preprocess_string(text, filters=filters) for text in documents]
-
-        # Tokenize, lowercase and remove stop words for query column in queries
-        queries = [preprocess_string(query, filters=filters) for query in queries]
+    # Tokenize, lowercase and remove stop words for query column in queries
+    queries = [preprocess_string(query, filters=filters) for query in queries]
 
     return (documents, ground_truth, queries)
+
+
+# Pre-process documents (list)
+def preprocess_documents(documents):
+    
+    processed_documents = list()
+    
+    for document in documents:
+        processed_documents.append(preprocess_string(document, filters=filters))
+    
+    return processed_documents
