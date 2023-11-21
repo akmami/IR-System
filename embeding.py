8faa2684cb56f71f01422bc84056f456dc94d9d8
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2023-11-22 01:17:52 +0300

    code refactor, logs added

--- embeding.py
+++ embeding.py
@@ -2,35 +2,49 @@ import logging
 import os
 import numpy as np
 import pandas as pd
-import csv
 from gensim.models import KeyedVectors
 from preprocess import read_dataset, preprocess_documents
 
 
-# Lower logging level
+# Lower logging level to give info to user
 logging.getLogger().setLevel(logging.INFO)
-
 
 # Get current directory
 cwd = os.getcwd()
 
-
 # Define embeding vectors' directory 
 DOCUMENTS_EMBEDDING_WORD2VEC_DIR = os.path.join(cwd, "dataset", "documents_embedding_word2vec.csv")
 DOCUMENTS_EMBEDDING_PRETRAINED_DIR = os.path.join(cwd, "dataset", "documents_embedding_pretrained.csv")
+WORD2VEC_MODEL_DIR = os.path.join(cwd, "model", "word2vec.model")
+WORD2VEC_PRETRAINED_MODEL_DIR = os.path.join(cwd, "model", "word2vec-google-news-300", "word2vec-google-news-300.gz")
 
+# Validate models
+exists = os.path.exists(WORD2VEC_MODEL_DIR)
+exists = exists and os.path.exists(WORD2VEC_PRETRAINED_MODEL_DIR)
 
-word2vec = KeyedVectors.load_word2vec_format("model/word2vec.model", binary=True)
-pre_trained = KeyedVectors.load_word2vec_format("model/word2vec-google-news-300/word2vec-google-news-300.gz", binary=True)
+if not exists:
+    logging.error("Models are not complete. Please make sure that Word2Vec and pre-trained one exists under model directory.")
+    exit(0)
 
+logging.info("Loading models.")
 
-# Lower logging level
+# Increase logging level to prevent gensim logs
+logging.getLogger().setLevel(logging.WARN)
+
+word2vec = KeyedVectors.load_word2vec_format(WORD2VEC_MODEL_DIR, binary=True)
+pre_trained = KeyedVectors.load_word2vec_format(WORD2VEC_PRETRAINED_MODEL_DIR, binary=True)
+
+# Lower logging level to give info to user
 logging.getLogger().setLevel(logging.INFO)
 
+logging.info("Models loaded successfully.")
 
-# test_document = """Editions of the Dewey Decimal Classifications,"Comaromi, J.P.","   The present study is a history of the DEWEY Decimal Classification.  The first edition of the DDC was published in 1876, the eighteenth edition in 1971, and future editions will continue to appear as needed.  In spite of the DDC's long and healthy life, however, its full story has never been told.  There have been biographies of Dewey that briefly describe his system, but this is the first attempt to provide a detailed history of the work that more than any other has spurred the growth of librarianship in this country and abroad."""
 
-
+#-------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
+# MARK: Methods
+#-------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
 def embed(document, model="word2vec"):
     
     document_tokens = document
@@ -67,13 +81,24 @@ def embed(document, model="word2vec"):
 
 
 def get_embeddings(allowLoad=True):
-    
+
+    logging.info("get_embeddings function execution started.")
+
     if allowLoad:
         if not os.path.exists(DOCUMENTS_EMBEDDING_WORD2VEC_DIR) or not os.path.exists(DOCUMENTS_EMBEDDING_PRETRAINED_DIR):
             logging.info("Embeding Word vectors could not be located.")
         else:
+            logging.info("Embedings found in local directory.")
+            logging.info("Loading Word2Vec from {}.".format(DOCUMENTS_EMBEDDING_WORD2VEC_DIR))
+
             pd_w2v = np.loadtxt(DOCUMENTS_EMBEDDING_WORD2VEC_DIR, delimiter=",")
+
+            logging.info("Loading Pre-trained Word2Vec from {}.".format(DOCUMENTS_EMBEDDING_PRETRAINED_DIR))
+
             pd_pre = np.loadtxt(DOCUMENTS_EMBEDDING_PRETRAINED_DIR, delimiter=",")
+
+            logging.info("Models loaded.")
+            logging.info("get_embeddings function execution ended.")
 
             return (pd_w2v, pd_pre)
 
@@ -88,12 +113,16 @@ def get_embeddings(allowLoad=True):
         documents_embedding_word2vec.append(embed(document, model="word2vec"))
         documents_embedding_pretrained.append(embed(document, model="pretrained"))
 
-    logging.info("Embeddings retrieved")
+    logging.info("All documents embedded.")
 
     pd_w2v = pd.DataFrame(documents_embedding_word2vec)
     pd_pre = pd.DataFrame(documents_embedding_pretrained)
 
+    logging.info("Ebmeddings saved to {} and {}.".format(DOCUMENTS_EMBEDDING_WORD2VEC_DIR, DOCUMENTS_EMBEDDING_PRETRAINED_DIR))
+
     pd_w2v.to_csv(DOCUMENTS_EMBEDDING_WORD2VEC_DIR, index=False, header=False)
     pd_pre.to_csv(DOCUMENTS_EMBEDDING_PRETRAINED_DIR, index=False, header=False)
+
+    logging.info("get_embeddings function execution ended.")
 
     return (np.array(documents_embedding_word2vec), np.array(documents_embedding_pretrained))
