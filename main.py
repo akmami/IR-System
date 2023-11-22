a7f340867b07cc73971bf95df1fa17bb513c1cd3
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2023-11-22 04:28:10 +0300

    method refactored, bugs are fixed, eval is added

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import os
 import logging
 import pandas as pd
-from download import download
-from train import train
-from query import query_all
+
 
 # Lower logging level
 logging.getLogger().setLevel(logging.INFO)
@@ -16,20 +14,34 @@ logging.getLogger().setLevel(logging.INFO)
 #-------------------------------------------------------------------------------------------
 def main():
 
+    from download import download
+    
     # Download dataset
     # If dataset is already installed, then the command will be ignored
     download()
 
+    from train import train
+    
     train(check=True)
 
-    ranks = query_all()
+    from query import query_all
+
+    query_results = query_all(weight_tfidf=0, weight_bm25=1, weight_w2v=0, weight_pre=0)
+    docs = [[result[0] for result in query_result] for query_result in query_results]
 
     cwd = os.getcwd()
-    RANKS = os.path.join(cwd, "ranks.csv")
+    QUERY_RESULTS = os.path.join(cwd, "query_results.csv")
+    DOCS = os.path.join(cwd, "docs.csv")
 
     # save ranks
-    pd_ranks = pd.DataFrame(ranks)
-    pd_ranks.to_csv(RANKS, index=False, header=False)
+    pd_query_results = pd.DataFrame(query_results)
+    pd_query_results.to_csv(QUERY_RESULTS, index=False, header=False)
+    pd_docs = pd.DataFrame(docs)
+    pd_docs.to_csv(DOCS, index=False, header=False)
+
+    from evaluation import evaluate
+
+    evaluate()
 
 
 #-------------------------------------------------------------------------------------------
