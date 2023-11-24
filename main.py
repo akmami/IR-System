20e12be3f6de84afbbc2cbb60bdb4c73f3f1f65c
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2023-11-24 12:34:57 +0300

    experiment extended with different weights for IRS

--- main.py
+++ main.py
@@ -26,22 +26,60 @@ def main():
 
     from query import query_all
 
-    query_results = query_all(weight_tfidf=0, weight_bm25=1, weight_w2v=0, weight_pre=0)
-    docs = [[result[0] for result in query_result] for query_result in query_results]
+    ensemble_weights = [(1.0, 0.0, 0.0, 0.0),
+                        (0.0, 1.0, 0.0, 0.0),
+                        (0.0, 0.0, 1.0, 0.0),
+                        (0.0, 0.0, 0.0, 1.0),
+                        (1.0, -0.1, 0.0, 0.0),
+                        (1.0, -0.2, 0.0, 0.0),
+                        (1.0, -0.3, 0.0, 0.0),
+                        (1.0, -0.4, 0.0, 0.0),
+                        (1.0, 0.0, -0.1, 0.0),
+                        (1.0, 0.0, -0.2, 0.0),
+                        (1.0, 0.0, -0.3, 0.0),
+                        (1.0, 0.0, -0.4, 0.0),
+                        (1.0, 0.0, 0.0, -0.1),
+                        (1.0, 0.0, 0.0, -0.2),
+                        (1.0, 0.0, 0.0, -0.3),
+                        (1.0, 0.0, 0.0, -0.4),
+                        (1.0, 0.0, 0.0, 0.1),
+                        (1.0, 0.0, 0.0, 0.2),
+                        (1.0, 0.0, 0.0, 0.3),
+                        (1.0, 0.0, 0.0, 0.4),
+                        (1.0, -0.4, 0.0, 0.1),
+                        (1.0, -0.4, 0.0, 0.2),
+                        (1.0, -0.4, 0.0, 0.3),
+                        (1.0, -0.4, 0.1, 0.0),
+                        (1.0, -0.4, 0.2, 0.0),
+                        (1.0, -0.4, 0.3, 0.0),
+                        (1.0, -0.4, 0.0, 0.0),
+                        (1.0, -0.4, 0.0, -0.1),
+                        (1.0, -0.4, 0.0, -0.2),
+                        (1.0, -0.4, 0.0, 0.0),
+                        (1.0, -0.4, -0.1, 0.0),
+                        (1.0, -0.4, -0.2, 0.0),
+                        (1.0, -0.4, 0.1, 0.1)]
 
-    cwd = os.getcwd()
-    QUERY_RESULTS = os.path.join(cwd, "query_results.csv")
-    DOCS = os.path.join(cwd, "docs.csv")
+    for weights in ensemble_weights:
 
-    # save ranks
-    pd_query_results = pd.DataFrame(query_results)
-    pd_query_results.to_csv(QUERY_RESULTS, index=False, header=False)
-    pd_docs = pd.DataFrame(docs)
-    pd_docs.to_csv(DOCS, index=False, header=False)
+        query_results = query_all(weight_tfidf=weights[0], weight_bm25=weights[1], weight_w2v=weights[2], weight_pre=weights[3])
+        docs = [[result[0] for result in query_result] for query_result in query_results]
 
-    from evaluation import evaluate
+        cwd = os.getcwd()
+        QUERY_RESULTS = os.path.join(cwd, "query_results.csv")
+        DOCS = os.path.join(cwd, "docs.csv")
 
-    evaluate()
+        # save ranks
+        pd_query_results = pd.DataFrame(query_results)
+        pd_query_results.to_csv(QUERY_RESULTS, index=False, header=False)
+        pd_docs = pd.DataFrame(docs)
+        pd_docs.to_csv(DOCS, index=False, header=False)
+
+        from evaluation import evaluate
+
+        print(weights)
+
+        evaluate(logs=logging.WARN)
 
 
 #-------------------------------------------------------------------------------------------
